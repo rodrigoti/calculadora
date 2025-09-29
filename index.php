1a7fc5529c0bfc368f83d08009fafbc2f78b48c1
Rodrigo TI <rodrigo@rodrigoti.com.br>
2025-09-28 21:21:30 -0300

    Novo title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora Básica Teste</title>
+    <title>Calculadora Básica</title>
     <style>
         body {
             font-family: Arial, sans-serif;
